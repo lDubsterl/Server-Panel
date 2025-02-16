44ae84489991e364b2af1a3f9e3ab8fad0f5a7ac
Dubster <shevc9@mail.ru>
2025-02-17 00:00:08 +0300

    Naming fixes and htpasswd mode changed to script executing

--- Panel.Infrastructure/Services/FtpManager.cs
+++ Panel.Infrastructure/Services/FtpManager.cs
@@ -18,9 +18,9 @@ namespace Panel.Infrastructure.Services
 
 		public string ManageFTPUser(string username, ManageMode mode)
 		{
-			var modeString = mode == 0 ? "-d" : "-D";
+			var modeString = mode == ManageMode.Create ? "-d" : "-D";
 			var password = RandomNumberGenerator.GetBytes(12).ToString()!;
-			_processManager.ExecuteCommand($"htpasswd -b {modeString} /etc/vsftpwd {username} {password}");
+			_processManager.ExecuteCommand($"htpasswd -i {modeString} /etc/vsftpwd {username} {password}");
 			return password;
 		}
 	}

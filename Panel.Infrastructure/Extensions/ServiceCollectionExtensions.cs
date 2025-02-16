cf929c857087958e14f3a719be62328ddf24fe6d
Dubster <shevc9@mail.ru>
2025-02-17 00:02:02 +0300

    Extension for services DI registration was added

--- Panel.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Panel.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,14 +1,22 @@
 ﻿using Panel.Infrastructure.Services;
 using MediatR;
 using Panel.Application.Interfaces.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Panel.Domain.Interfaces.Repositories;
+using Panel.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Panel.Infrastructure.Hubs;
 
 namespace Panel.Infrastructure.Extensions
 {
 	public static class ServiceCollectionExtensions
 	{
-		public static void AddInfrastructureLayer(this IServiceCollection services)
+		public static void AddInfrastructureLayer(this IServiceCollection services, IConfiguration config)
 		{
 			services.AddServices();
+			services.AddDbContext(config);
+			services.AddRepositories();
 		}
 
 		private static void AddServices(this IServiceCollection services)
@@ -16,7 +24,25 @@ namespace Panel.Infrastructure.Extensions
 			services
 				.AddTransient<IMediator, Mediator>()
 				.AddTransient<IAuthenticationService, AuthenticationService>()
-				.AddTransient<ITokenService, TokenService>();
+				.AddTransient<ITokenService, TokenService>()
+				.AddTransient<IConsoleHub, ConsoleHub>()
+				.AddSingleton<IFtpManager, FtpManager>()
+				.AddSingleton<IProcessManager, ProcessManager>();
+		}
+
+		public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connectionString = configuration["DatabaseConnectionString"];
+
+			services.AddDbContext<PanelDbContext>(options =>
+			   options.UseNpgsql(connectionString));
+		}
+
+		private static void AddRepositories(this IServiceCollection services)
+		{
+			services
+				.AddTransient<IUnitOfWork, UnitOfWork>()
+				.AddTransient(typeof(IRepository<>), typeof(Repository<>));
 		}
 	}
 }

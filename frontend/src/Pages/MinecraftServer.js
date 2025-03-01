3ed38a5887b19399612c736a55befed38696c162
Dubster <shevc9@mail.ru>
2025-03-01 16:10:55 +0300

    Minecraft server page has been created

--- frontend/src/Pages/MinecraftServer.js
+++ frontend/src/Pages/MinecraftServer.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import styles from '../styles/MinecraftConsole.module.css';
+import styles from '../styles/ServerPage.module.css';
+import ServerConsole from '../components/ServerConsole';
+import TabButtons from '../components/TabButtons';
+import MinecraftSettings from '../components/MinecraftSettings';
+
 const MinecraftServer = () => {
+    const [selectedButton, setSelected] = useState('consoleBtnH');
+
+    useEffect(() => { document.getElementById(selectedButton).style.opacity = 1; }, []);
+
     return (
-        <div className={styles["console-container"]}>
-            <textarea className={styles["console-output"]} readOnly placeholder={styles["Console output..."]} />
-            <div className={styles["console-input-container"]}>
-                <input type="text" className={styles["console-input"]} placeholder="Enter command..." />
-                <button className={styles["console-button"]}>Send</button>
+        <div className={styles["background"]}>
+            <TabButtons selectedButton={selectedButton} setSelected={setSelected} />
+            <div className={styles["content"]}>
+                    {selectedButton === 'consoleBtnH' && <ServerConsole />}
+                    {selectedButton === 'settingsBtnH' && <MinecraftSettings/>}
             </div>
         </div>
     );

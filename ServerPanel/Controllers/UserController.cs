fa28e2676622d2b466d96832d8e49788d0c91216
Dubster <shevc9@mail.ru>
2025-04-20 13:16:25 +0300

    ServerController enpoints's namings have been changed

---
+++ ServerPanel/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Panel.Domain.Interfaces.Repositories;
+using Panel.Domain.Models;
+using ServerPanel.Controllers;
+using System.Threading.Tasks;
+
+namespace Panel.WebAPI.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	[Authorize]
+	public class UserController : BaseController
+	{
+		private IUnitOfWork _unitOfWork;
+
+		public UserController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetServers()
+		{
+			var user = await _unitOfWork.Repository<User>().GetByIdAsync(UserId);
+			return Ok(new { 
+				minecraft = !string.IsNullOrEmpty(user.MinecraftServerExecutable),
+				dst = user.DSTServer,
+				terraria = user.TerrariaServer
+			});
+		}
+	}
+}

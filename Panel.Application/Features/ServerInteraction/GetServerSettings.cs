220d709e59b4e623b046424b2ca733f1ce82c3d1
Dubster <shevc9@mail.ru>
2025-04-20 13:08:20 +0300

    Multiple servers support has been added

--- Panel.Application/Features/ServerInteraction/GetServerSettings.cs
+++ Panel.Application/Features/ServerInteraction/GetServerSettings.cs
@@ -10,27 +10,47 @@ namespace Panel.Application.Features.ServerInteraction
 {
 
 	public class GerServerSettingsHandler : IRequestHandler<GetServerSettingsRequest, IActionResult>
-    {
-        IUnitOfWork _unitOfWork;
-        IConfiguration _configuration;
+	{
+		IUnitOfWork _unitOfWork;
+		IConfiguration _configuration;
 
-        public GerServerSettingsHandler(IUnitOfWork unitOfWork, IConfiguration configuration)
-        {
-            _unitOfWork = unitOfWork;
-            _configuration = configuration;
-        }
+		public GerServerSettingsHandler(IUnitOfWork unitOfWork, IConfiguration configuration)
+		{
+			_unitOfWork = unitOfWork;
+			_configuration = configuration;
+		}
 
-        public async Task<IActionResult> Handle(GetServerSettingsRequest request, CancellationToken cancellationToken)
-        {
-            var user = await _unitOfWork.Repository<User>().GetByIdAsync(request.UserId);
-            if (user == null)
-                return new NotFoundObjectResult(new BaseResponse(false, "There is no user with such id"));
+		public async Task<IActionResult> Handle(GetServerSettingsRequest request, CancellationToken cancellationToken)
+		{
+			var user = await _unitOfWork.Repository<User>().GetByIdAsync(request.UserId);
+			if (user == null)
+				return new NotFoundObjectResult(new BaseResponse(false, "There is no user with such id"));
 
-            if (string.IsNullOrEmpty(user.MinecraftServerExecutable))
-                return new NoContentResult();
+			if (request.ServerType == ServerTypes.Minecraft)
+				return GetMinecraftSettings(user);
+			if (request.ServerType == ServerTypes.DstMaster)
+				return GetDSTSettings(user.DSTServer, user.Email);
+			return new NotFoundObjectResult(new BaseResponse(false, "There is no servers with this type"));
+		}
 
-            var settingsDirectory = $"{_configuration["ServersDirectory"]}/{user.Email.Replace("@", "")}/Minecraft/server.properties";
-            return new OkObjectResult(File.ReadAllLines(settingsDirectory));
-        }
-    }
+		private IActionResult GetMinecraftSettings(User user)
+		{
+			if (string.IsNullOrEmpty(user.MinecraftServerExecutable))
+				return new NoContentResult();
+
+			var settingsDirectory = $"{_configuration["ServersDirectory"]}/{user.Email.Replace("@", "")}/Minecraft/server.properties";
+			return new OkObjectResult(File.ReadAllLines(settingsDirectory));
+		}
+		private OkObjectResult GetDSTSettings(bool isCreated, string email)
+		{
+			var settingsDirectory = $"{_configuration["ServersDirectory"]}/{email.Replace("@", "")}/DST/Master/worldgenoverride.lua";
+			if (isCreated)
+				return new OkObjectResult(File.ReadAllLines(settingsDirectory));
+			else
+			{
+				settingsDirectory = $"{_configuration["ServersDirectory"]}/DST templates/master_worldgen.txt";
+				return new OkObjectResult(File.ReadAllLines(settingsDirectory));
+			}
+		}
+	}
 }
--- Panel.Application/Features/ServerInteraction/GetServerStatus.cs
+++ Panel.Application/Features/ServerInteraction/GetServerStatus.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Panel.Application.Interfaces.Services;
+using Panel.Domain.Common;
 using Panel.Domain.Interfaces.Repositories;
 using Panel.Domain.Models;
 
@@ -10,36 +11,60 @@ namespace Panel.Application.Features.ServerInteraction
 	public class GetServerStatusRequest : IRequest<IActionResult>
 	{
 		public int UserId { get; set; }
+		public ServerTypes ServerType { get; set; }
 
-		public GetServerStatusRequest(int id)
+		public GetServerStatusRequest(int id, ServerTypes type)
 		{
 			UserId = id;
+			ServerType = type;
 		}
 	}
 
 	public class GetServerStatusHandler : IRequestHandler<GetServerStatusRequest, IActionResult>
 	{
 		IUnitOfWork _unitOfWork;
-		IOsInteractionsService _processManager;
+		IOsInteraction _processManager;
 
-		public GetServerStatusHandler(IUnitOfWork unitOfWork, IOsInteractionsService proceeManager)
+		public GetServerStatusHandler(IUnitOfWork unitOfWork, IOsInteraction processManager)
 		{
 			_unitOfWork = unitOfWork;
-			_processManager = proceeManager;
+			_processManager = processManager;
 		}
 
 		public async Task<IActionResult> Handle(GetServerStatusRequest request, CancellationToken cancellationToken)
 		{
-			var server = await _unitOfWork.Repository<RunningServer>().Entities.Where(s => s.UserId == request.UserId).FirstOrDefaultAsync();
+			var server = await _unitOfWork.Repository<RunningServer>().Entities
+				.Where(s => s.UserId == request.UserId && s.ServerType == request.ServerType)
+				.FirstOrDefaultAsync();
+			var user = await _unitOfWork.Repository<User>().GetByIdAsync(request.UserId);
 
+			if (user == null)
+				return new BadRequestObjectResult(new BaseResponse(false, "User not found"));
+			bool isExists = request.ServerType switch
+			{
+				ServerTypes.DstMaster => user.DSTServer,
+				ServerTypes.Terraria => user.TerrariaServer,
+				ServerTypes.Minecraft => !string.IsNullOrEmpty(user.MinecraftServerExecutable),
+				_ => false
+			};
+			if (!isExists)
+				return new BadRequestResult();
 			if (server == null)
 				return new OkObjectResult(new { isStarted = false });
-			return new OkObjectResult(new
-			{
-				isStarted = true,
-				address = _processManager.ExecuteCommand("curl ifconfig.me") + ":" + server.Port,
-				containerName = server.ContainerName
-			});
+
+			if (request.ServerType == ServerTypes.Minecraft || request.ServerType == ServerTypes.Terraria)
+				return new OkObjectResult(new
+				{
+					isStarted = true,
+					address = _processManager.ExecuteCommand("curl ifconfig.me") + ":" + server.Port,
+					containerName = server.ContainerName
+				});
+			else
+				return new OkObjectResult(new
+				{
+					isStarted = true,
+					containerName = server.ContainerName
+				});
 		}
 	}
 }

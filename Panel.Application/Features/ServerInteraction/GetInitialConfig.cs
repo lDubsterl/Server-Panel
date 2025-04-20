4b81eff3c2b99d5a4c7114531e8f86146be42f1c
Dubster <shevc9@mail.ru>
2025-04-20 13:07:42 +0300

    Servers' base config's getter has been added

---
+++ Panel.Application/Features/ServerInteraction/GetInitialConfig.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Panel.Domain.Common;
+
+namespace Panel.Application.Features.ServerInteraction
+{
+	public class GetConfigRequest : IRequest<IActionResult>
+	{
+		public ServerTypes ServerType { get; set; }
+	}
+	public class GetInitialConfig(IConfiguration config) : IRequestHandler<GetConfigRequest, IActionResult>
+	{
+		IConfiguration _config = config;
+
+		public async Task<IActionResult> Handle(GetConfigRequest request, CancellationToken cancellationToken)
+		{
+			var result = request.ServerType switch
+			{
+				ServerTypes.DstMaster => File.ReadAllText(_config["ServersDirectory"] + "/DST templates/master_worldgen.txt"),
+				ServerTypes.Terraria => File.ReadAllText(_config["ServersDirectory"] + "/Terraria/tModLoader/serverconfig.txt"),
+				_ => ""
+			};
+			return await Task.FromResult(new OkObjectResult(result));
+		}
+	}
+}
